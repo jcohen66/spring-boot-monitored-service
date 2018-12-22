0864600621b53e19fe1bdcbf5c4174944f1a24fa
jcohen66 <jcohen66@optonline.net>
2018-12-21 20:41:46 -0500

    Added security

--- src/main/java/com/example/clientdemo/web/WebController.java
+++ src/main/java/com/example/clientdemo/web/WebController.java
@@ -1,11 +1,16 @@
 package com.example.clientdemo.web;
 
+import org.json.simple.JSONObject;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
 
 class MyApplicationException extends Exception {
 
@@ -18,6 +23,18 @@ public class WebController {
         return new ResponseEntity<>("Spring Client Application",  HttpStatus.OK);
     }
 
+    @GetMapping(path = "/entity", produces= MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<Object> sayHello()
+    {
+        //Get data from service layer into entityList.
+
+        List<JSONObject> entities = new ArrayList<JSONObject>();
+            JSONObject entity = new JSONObject();
+            entity.put("aa", "bb");
+            entities.add(entity);
+
+        return new ResponseEntity<Object>(entities, HttpStatus.OK);
+    }
 
     @ExceptionHandler(MyApplicationException.class)
     @ResponseStatus(HttpStatus.BAD_REQUEST)
